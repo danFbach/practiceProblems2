976982ef9723d89ec1894225e334abf39f5edc49
danFbach <quiktekrepair@gmail.com>
2016-03-22 12:14:39 -0500

    problem 1 Check

---
+++ practiceProblems2/wordCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceProblems2
+{
+    public class wordCheck
+    {
+        public string userInput;
+        public List<string> listOfWords = new List<string>();
+        public void getNewWord()
+        {
+            Console.WriteLine("Please enter a word.");
+            userInput = Console.ReadLine();
+
+        }
+
+        public void checkWord()
+        {
+            foreach(string word in listOfWords)
+                {
+                if (userInput == word)
+                {
+                    Console.WriteLine("BARK!");
+                }
+
+            }Console.WriteLine("Nice word dude.");
+            listOfWords.Add(userInput);
+        }
+    }
+}
